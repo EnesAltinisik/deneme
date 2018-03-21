632074f397d64192a7f0a59ea67eeb9d5f44d096
Unknown <ugurtufekci.tobbetu@gmail.com>
2018-03-21 16:51:25 +0300

    Checking answers snapshots,screenshots

---
+++ InternshipER/App_Code/AnswerCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace InternshipER.App_Code
+{
+    public class AnswerCheck
+    {
+        string[] filePaths = Directory.GetFiles(@"C:\Internshiper\snapshot\flag\");
+
+
+        public void print()
+        {
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                string path = filePaths[i];
+                Console.WriteLine(System.IO.Path.GetFileName(path));
+            }
+        }
+
+        
+
+
+    }
+
+}
+
